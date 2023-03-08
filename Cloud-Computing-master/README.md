d3b52cb2dc2fb821353d6d8536dea395fd290af6
Ilham Ariatama <88831950+ARTPHD@users.noreply.github.com>
2023-03-08 09:31:35 +0700

    Add files via upload

---
+++ Cloud-Computing-master/README.md
@@ -0,0 +1,27 @@
+# Cloud-Computing
+REST API for tanya mobile app <br/>
+Base url: https://tanya-ps278.et.r.appspot.com/ <br/>
+Api documentation: https://tanya-ps278.et.r.appspot.com/tanyaDocs <br/>
+
+* access tanya API endpoint: *baseURL/tanya/[endpoint]*
+
+-----------------------------------------------------------------------
+
+## Create REST APIs
+1. Create database using mongoDB atlas, then get access key
+2. install the packages contained in the package.json file
+3. Using jsonwebtoken for authorization
+
+## Create API documentation using Swagger
+1. run $ npm install swagger-ui-express
+2. export folder collection in Postman
+3. save into tanya APIs folder (Tanya-PS278.json)
+4. upload json file result from postman export to transform API [here](https://www.apimatic.io/dashboard?modal=transform). Convert to OpenAPI/Swagger v2.0 (JSON) format
+5. save the converted file into tanya APIs folder (tanyaDocs.json)
+6. create /tanyaDocs endpoint inside index.js
+
+## Deploy tanya APIs to Google App Engine
+1. install Google SDK
+2. create app.yaml 
+3. deploy the app
+5. and get the URL to access API endpoints
---
+++ Cloud-Computing-master/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const swagger = require("swagger-ui-express");
+dotenv.config();
+
+// test
+app.get("/", (req, res) => {
+  res.send("Hello from Tanya!");
+});
+
+// swagger
+const apiDocs = require("./tanyaDocs.json");
+app.use("/tanyaDocs", swagger.serve, swagger.setup(apiDocs));
+
+//connect DB
+mongoose.connect(process.env.access_db, () => console.log("check database"));
+
+//import route
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const tiketRoute = require("./routes/Tiket");
+const transaksiRoute = require("./routes/transaksi");
+const busConditionRoute = require("./routes/busCondition");
+const prediksiRoute = require("./routes/prediksi");
+//driver and staff route
+const authdriverRoute = require("./routes/authDriver");
+const driverRoute = require("./routes/driver");
+
+//middleware | run when hits routes
+app.use(express.json());
+
+//route middleware
+app.use("/tanya/user", userRoute);
+app.use("/tanya/auth", authRoute);
+app.use("/tanya/tiket", tiketRoute);
+app.use("/tanya/transaksi", transaksiRoute);
+app.use("/tanya/busCondition", busConditionRoute);
+app.use("/tanya/authDriver", authdriverRoute);
+app.use("/tanya/driver", driverRoute);
+app.use("/tanya/prediksi", prediksiRoute);
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
